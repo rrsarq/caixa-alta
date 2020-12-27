cd5ec4e8587d27054ec47f1d60c074670923ddbc
rrsarq <25100491+rrsarq@users.noreply.github.com>
2020-12-27 15:10:45 -0300

    Adicionada opção de sair

--- pycaps.py
+++ pycaps.py
@@ -1,24 +1,48 @@
-repetir = 0
-option = 0
+# Definição das variáveis para o loop do programa
+repetir = 1
+opcao = 0
+
+# Definição das funções maiúsculo e minúsculo 
 
 def maiusculo(texto):
     texto=input("Cole aqui texto para deixar em maiúsculo: ")
     caps = texto.upper()
+    print("")
+    print("Selecione e copie o texto modificado:")
     print(caps)
-
+    print("")
+   
 def minusculo(texto):
     texto=input("Cole aqui texto para deixar em minúsculo: ")
     small = texto.lower()
+    print("")
+    print("Selecione e copie o texto modificado:")
     print(small)
+    print("")
+    
 
-while repetir == 0:
-    while option == 0:
-        option=input("Escolha 1-MAIUSCULO 2-minusculo: ") 
-        if option == str(1):
+while repetir == 1:
+    while opcao == 0:
+        opcao=input("Escolha 1-MAIUSCULO ou 2-minusculo: ") 
+        if opcao == str(1):
+            print("OK! Tecle Enter:")
             maiusculo(input())
-            option = 0
-        if option == str(2):
+            sair = input("Digite S para sair ou qualquer outra para continuar: ")
+            if sair == "S":
+                repetir = 0
+                exit()
+            else:
+                opcao = 0
+                print("---")
+        if opcao == str(2):
+             print("OK! Tecle Enter:")
              minusculo(input())
-             option = 0
+             sair = input("Digite S para sair ou qualquer outra para continuar: ")
+             if sair == "S":
+                repetir = 0
+                exit()
+             else:
+                opcao = 0
+                print("---")
         else:
-             option=0
+             opcao = 0
